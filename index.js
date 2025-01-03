b1978fd6672be7af3acf465664b3f0188d2388f4
bvillegas23 <braulio.villegas2019@gmail.com>
2025-01-02 23:13:27 -0500

    Exercise 07

---
+++ index.js
@@ -0,0 +1,19 @@
+// Secure the Vault - Workshop 07 
+
+// User Message 
+const message = "You have received this message because you have been chosen to open an important vault.Here is the secret combination:"
+
+// Set Secret Code 1 
+const secretValue1 = 12 + 17;
+// Set Secret Code 2 
+const secretValue2 = 100 * 5;
+// Set Secret Code 3 
+const secretValue3 = 200 - 50;
+
+//Returns values in console to verify codes .
+console.log(secretValue1);
+console.log(secretValue2);
+console.log(secretValue3);
+
+//Alert to returns message and code values .
+alert(`${message} ${secretValue1}-${secretValue2}-${secretValue3}`);
